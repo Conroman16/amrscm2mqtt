91a631407c0085a575f011cb984ce0c127e6cd3c
Connor Kennedy <conroman16@users.noreply.github.com>
2025-08-07 18:11:56 -0500

    Add ability to read settings from env

--- settings_template.py
+++ settings_template.py
@@ -1,8 +1,14 @@
+import os
+
 # List of the Meter IDs to watch
 # Use empty brackets to read all meters - []
 # List may contain only one entry - [12345678]
 # or multiple entries - [12345678, 98765432, 12340123]
-WATCHED_METERS = []
+env_meters = os.getenv('WATCHED_METERS')
+if env_meters:
+    WATCHED_METERS = [m.strip() for m in env_meters.split(',')]
+else:
+    WATCHED_METERS = []
 
 # MQTT Server settings
 # MQTT_HOST needs to be a string
@@ -10,13 +16,13 @@ WATCHED_METERS = []
 # MQTT_USER needs to be a string
 # MQTT_PASSWORD needs to be a string
 # If no authentication, leave MQTT_USER and MQTT_PASSWORD empty
-MQTT_HOST = '127.0.0.1'
-MQTT_PORT = 1883
-MQTT_USER = ''
-MQTT_PASSWORD = ''
+MQTT_HOST = os.getenv('MQTT_HOST', '127.0.0.1')
+MQTT_PORT = int(os.getenv('MQTT_PORT', '1883'))
+MQTT_USER = os.getenv('MQTT_USER', '')
+MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', '')
 
 # path to rtlamr
-RTLAMR = '/usr/local/bin/rtlamr'
+RTLAMR = os.getenv('RTLAMR_PATH', '/usr/local/bin/rtlamr')
 
 # path to rtl_tcp
-RTL_TCP = '/usr/bin/rtl_tcp'
+RTL_TCP = os.getenv('RTL_TCP_PATH', '/usr/bin/rtl_tcp')
